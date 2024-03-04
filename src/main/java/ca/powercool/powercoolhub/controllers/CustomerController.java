d1e0b40b21cda8ef16d2df20871cc72efc66d831
Rishabh Verma <rishabhverma2503@gmail.com>
2024-03-04 15:15:25 -0800

    changed from REST to regular controller

--- src/main/java/ca/powercool/powercoolhub/controllers/CustomerController.java
+++ src/main/java/ca/powercool/powercoolhub/controllers/CustomerController.java
@@ -1,34 +1,17 @@
 package ca.powercool.powercoolhub.controllers;
 
-import java.lang.ProcessBuilder.Redirect;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import ca.powercool.powercoolhub.models.User;
-import ca.powercool.powercoolhub.repositories.CustomerRepository;
-import ca.powercool.powercoolhub.repositories.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.util.List;
 import java.util.Optional;
-
 import ca.powercool.powercoolhub.models.Customer;
+import ca.powercool.powercoolhub.repositories.CustomerRepository;
 
-@RestController
+@Controller
 @RequestMapping("/customers")
 public class CustomerController {
 
@@ -36,15 +19,14 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping("/viewAll")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
+    public String viewAllCustomers(Model model) {
         List<Customer> customers = customerRepository.findAll();
-        
-        return new ResponseEntity<>(customers, HttpStatus.OK);
+        model.addAttribute("customers", customers);
+        return "viewAll"; // Return the name of the Thymeleaf template
     }
 
-
-
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
         Optional<Customer> customerOptional = customerRepository.findById(id);
         return customerOptional.map(customer -> new ResponseEntity<>(customer, HttpStatus.OK))
@@ -56,5 +38,27 @@ public class CustomerController {
         Customer createdCustomer = customerRepository.save(customer);
         return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
     }
-}
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (customerOptional.isPresent()) {
+            customer.setId(id);
+            Customer updatedCustomer = customerRepository.save(customer);
+            return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Integer id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (customerOptional.isPresent()) {
+            customerRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
