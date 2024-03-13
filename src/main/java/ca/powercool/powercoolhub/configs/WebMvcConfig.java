346c23b3e5782c1c0caba0212a3ab84652f14d5e
Aki <AkkarachaiWangcharoensap@hotmail.com>
2024-03-12 21:36:10 -0700

    Fixed: empty Date object causes internal server error when updating customers

--- src/main/java/ca/powercool/powercoolhub/configs/WebMvcConfig.java
+++ src/main/java/ca/powercool/powercoolhub/configs/WebMvcConfig.java
@@ -2,9 +2,11 @@ package ca.powercool.powercoolhub.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import ca.powercool.powercoolhub.converters.StringToDateConverter;
 import ca.powercool.powercoolhub.interceptors.RoleBasedAccessInterceptor;
 
 @Configuration
@@ -21,5 +23,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(roleAccessInterceptor)
                 .addPathPatterns("/users/manager/**", "/users/employee/**", "/customers/**");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToDateConverter());
     }
 }
