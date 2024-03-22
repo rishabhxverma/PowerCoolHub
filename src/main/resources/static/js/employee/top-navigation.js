c93d32f16557499e6904329e071af4c4b44223e0
Alex Menzies <menzies23@gmail.com>
2024-03-22 12:11:26 -0700

    added google maps API

--- src/main/resources/static/js/employee/top-navigation.js
+++ src/main/resources/static/js/employee/top-navigation.js
@@ -1,3 +1,6 @@
+
+let buttonClicked = false;
+
 function toggle_clock(event, button) {
     // Prevent the default behavior of the event
     event.preventDefault();
@@ -8,5 +11,44 @@ function toggle_clock(event, button) {
     icon.classList.toggle('text-danger');
 
     // Toggle fa-fade class
-    icon.classList.toggle('fa-fade');;
+    icon.classList.toggle('fa-fade');
+    let coordinatesDiv = document.getElementById('coordinates');
+    // Check if button was clicked before
+    if (buttonClicked) {
+        // The button was clicked before, attempt to display current location coordinates and time
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                // Get the coordinates of the current position
+                let lat = position.coords.latitude;
+                let lng = position.coords.longitude;
+
+                // Get the current time
+                let now = new Date();
+                let clockoutTime = now.toLocaleTimeString(); 
+
+                // Update the coordinates container with the location and time
+                coordinatesDiv.innerText += ' Latitude: ' + lat + ', Longitude: ' + lng + ', Clock Out: ' + clockoutTime;
+
+                // Make the coordinates div visible
+                coordinatesDiv.style.display = 'block';
+            }, function(error) {
+                // Handle location error
+                console.error('Geolocation error: ' + error.message);
+                let coordinatesDiv = document.getElementById('coordinates');
+                coordinatesDiv.innerText = 'Error getting location: ' + error.message;
+                coordinatesDiv.style.display = 'block'; // Show the div to display the error message
+            });
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+            let coordinatesDiv = document.getElementById('coordinates');
+            coordinatesDiv.innerText = 'Geolocation is not supported by this browser.';
+            coordinatesDiv.style.display = 'block'; // Show the div even if geolocation is not supported
+        }
+    } else {
+        buttonClicked = true;
+        let clockInDate = new Date();
+        let clockInTime = clockInDate.toLocaleTimeString();
+        coordinatesDiv.innerText = 'Clock In: ' + clockInTime;
+
+    }
 }
