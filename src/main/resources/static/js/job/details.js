75150c6cdcead60bc5e00ce033aa6ef40533bc38
Aki <AkkarachaiWangcharoensap@hotmail.com>
2024-04-02 15:34:06 -0700

    Updated: refactored job completion button, Added: Technician and Service Manager can update the job note

--- src/main/resources/static/js/job/details.js
+++ src/main/resources/static/js/job/details.js
@@ -3,12 +3,14 @@ function on_complete_button_clicked(dom) {
     const HTTP_OK = 200;
 
     let button = dom;
+    let jobId = dom.getAttribute('jobId');
+    
     button.classList.add('disabled');
     button.setAttribute('disabled', 'disabled');
 
     fetchAndProcessLocation(address => {
         let xhr = new XMLHttpRequest();
-        xhr.open('POST', dom.getAttribute('jobId') + '/complete');
+        xhr.open('POST', jobId + '/complete');
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onreadystatechange = function () {
             // When response is returned.
@@ -26,13 +28,13 @@ function on_complete_button_clicked(dom) {
             }
         };
 
-        xhr.send(JSON.stringify({ address: address }));
+        xhr.send(address);
     })
 }
 
 function on_direction_button_clicked(dom) {
     let mapUrl;
-    let address = dom.getAttribute('data-address');
+    let address = dom.getAttribute('customerAddress');
 
     // Apple device, use Apple Maps
     if (/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) {
@@ -45,4 +47,25 @@ function on_direction_button_clicked(dom) {
 
     window.open(mapUrl, '_blank');
     // window.location.href = mapUrl;
+}
+
+function on_note_field_changed(dom) {
+    let jobId = dom.getAttribute('jobId');
+    let note = dom.value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', jobId + '/note/update');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+        // When response is returned.
+        if (xhr.readyState === READY_STATE_DONE) {
+            if (xhr.status === HTTP_OK) {
+                // Update status element if response is good
+            } else {
+                console.error('Request failed:', xhr.status);
+            }
+        }
+    };
+
+    xhr.send(note);
 }
