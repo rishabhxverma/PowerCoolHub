eb645e57e253fbf84913fcec9626c6fc3bfac6d8
Mahdi <mahdibeigahmadi@gmail.com>
2024-04-07 09:51:53 -0700

    added message for customer
    I added a field that managers can set message for customers while booking appointments.

--- src/main/java/ca/powercool/powercoolhub/controllers/JobController.java
+++ src/main/java/ca/powercool/powercoolhub/controllers/JobController.java
@@ -89,6 +89,7 @@ public class JobController {
     @PostMapping("/addJob")
     public ResponseEntity<String> addJobForTheCustomerIntoDataBase(
             @RequestParam("customerId") int customerIdInfo,
+            @RequestParam("message") String aMessageForCustomer,
             @RequestParam("dateService") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate serviceDate,
             @RequestParam(required = false, value = "message") String message,
             @RequestParam("note") String note,
@@ -114,6 +115,7 @@ public class JobController {
     
         customer.setState(Customer.CustomerState.UPCOMING);
         customer.setNextService(serviceDate);
+        customer.setMessage(aMessageForCustomer);
         customerRepository.save(customer);
     
         jobRepository.save(job);
@@ -364,6 +366,7 @@ public class JobController {
      */
     @PostMapping("/updateJob")
     public String updateJobInDatabase(@RequestParam("jobId") int jobId,
+                                    @RequestParam("message") String aMessageForCustomer,
                                     @RequestParam("dateService") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate serviceDate,
                                     @RequestParam(required = false, value = "message") String message,
                                     @RequestParam("note") String note,
@@ -394,6 +397,7 @@ public class JobController {
         if (customer != null) {
             customer.setState(Customer.CustomerState.UPCOMING);
             customer.setNextService(serviceDate);
+            customer.setMessage(aMessageForCustomer);
             customerRepository.save(customer);
         } else {
             // Handle case where customer is not found
@@ -443,5 +447,4 @@ public class JobController {
     
         return "redirect:/jobs/viewAllJobs";
     }
-
 }
