75011816ae50562dee5400b8187e531c128ef996
Rishabh Verma <rishabhverma2503@gmail.com>
2024-04-08 17:34:30 -0700

    fixed book appointment when going to /viewAll?filter=requestingApp

--- src/main/resources/static/js/viewAll.js
+++ src/main/resources/static/js/viewAll.js
@@ -1,5 +1,5 @@
-document.getElementById("filter").addEventListener("change", function () {
-  var selectedFilter = this.value;
+function handleFilterChange() {
+  var selectedFilter = document.getElementById("filter").value;
   fetch(`/customers/filterJson?filter=${selectedFilter}`)
     .then((response) => response.json())
     .then((data) => {
@@ -50,7 +50,15 @@ document.getElementById("filter").addEventListener("change", function () {
         tbody.appendChild(row);
       });
     });
-});
+}
+
+// Call the function when the page loads
+handleFilterChange();
+
+// Call the function when the filter changes
+document
+  .getElementById("filter")
+  .addEventListener("change", handleFilterChange);
 
 // Function to append cells
 function appendCell(row, element, className, href, textContent) {
@@ -68,7 +76,6 @@ function appendCell(row, element, className, href, textContent) {
   }
   row.appendChild(cell);
 }
-
 
 // Enables clicking on user boxes to check them..................
 let techBoxes = document.querySelectorAll(".tech-box");
