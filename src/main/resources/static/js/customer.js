0e9ce6e342353564ae5bf3e81384441b05d903e1
Mahdi <mahdibeigahmadi@gmail.com>
2024-04-08 19:07:38 -0700

    improved customer edit UI
    It wont bring you anymore to page that had confirm button

--- src/main/resources/static/js/customer.js
+++ src/main/resources/static/js/customer.js
@@ -1,8 +1,25 @@
-//functions for auto complete, and functions for validating the form
-// This file is used in the addCustomer.html and editCustomer.html
-
 function initAutocomplete() {
   var input = document.getElementById("address");
   var autocomplete = new google.maps.places.Autocomplete(input);
 }
 window.onload = initAutocomplete;
+function confirmUpdate(event) {
+  event.preventDefault();
+  if (confirm('Are you sure you want to update this customer?')) {
+      var form = $('#editCustomerForm');
+      $.ajax({
+          url: form.attr('action'),
+          type: 'POST',
+          data: form.serialize(),
+          success: function (response) {
+              alert(response.message);
+
+              window.location.href = '/customers/viewAll';
+          },
+          error: function (xhr, status, error) {
+              var errorMessage = xhr.responseJSON && xhr.responseJSON.error ? xhr.responseJSON.error : "An error occurred during the update request.";
+              alert(errorMessage);
+          }
+      });
+  }
+}
